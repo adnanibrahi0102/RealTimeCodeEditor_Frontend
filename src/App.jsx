758284ad1ea8d68666bc08063610704a585e9ffb
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-09-14 16:22:04 +0530

    Ready to begin

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className='flex justify-center items-center'>
+      <h1 className='text-4xl text-pink-600 font-bold '>Welcome to Realtime Code Editor</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
